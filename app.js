58fdb0a4bdcd5f9093742ecbe79b6c45bed673cb
Hemant Verma <vermahemant837@gmail.com>
2023-07-28 18:29:35 +0530

    forth commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-// Task 1 - Add Data in local storage
+let items = document.getElementById("items-group");
+let item = document.createElement("li");
 
 function logFormData(event) {
   event.preventDefault();
@@ -20,14 +21,20 @@ function logFormData(event) {
 
   localStorage.setItem(`${email}`, userData);
 
-  // Task 2 - Show User's Data on Webpage
-
   let items = document.getElementById("items-group");
   let item = document.createElement("li");
   let text = `Patient name: ${name} Email: ${email} Phone: ${phno} Date: ${date} Time: ${time}`;
 
-  item.textContent = text;
+  // Task 3 - Add a delete buton and delete functionality
+  let deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.className = "deleteButton";
 
+  item.setAttribute("data-email", email);
+
+  item.textContent = text;
+  item.addEventListener("click", deleteUser);
+  item.appendChild(deleteButton);
   items.appendChild(item);
 
   document.getElementById("name").value = "";
@@ -35,4 +42,13 @@ function logFormData(event) {
   document.getElementById("phno").value = "";
   document.getElementById("tfc-date").value = "";
   document.getElementById("tfc-time").value = "";
+}
+
+function deleteUser(event) {
+  if (confirm("Are You Sure?")) {
+    let li = event.target.parentElement;
+    let email = li.getAttribute("data-email");
+    localStorage.removeItem(email);
+    li.remove();
+  }
 }
