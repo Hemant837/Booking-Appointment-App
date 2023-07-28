f546e574d19d24dc7f7d07315b9f682073de3e3f
Hemant Verma <vermahemant837@gmail.com>
2023-07-28 16:39:00 +0530

    third commit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Task 1 - Add Data in local storage
+
 function logFormData(event) {
   event.preventDefault();
   let name = document.getElementById("name").value;
@@ -15,7 +17,18 @@ function logFormData(event) {
   };
 
   let userData = JSON.stringify(user);
-  localStorage.setItem("userData", userData);
+
+  localStorage.setItem(`${email}`, userData);
+
+  // Task 2 - Show User's Data on Webpage
+
+  let items = document.getElementById("items-group");
+  let item = document.createElement("li");
+  let text = `Patient name: ${name} Email: ${email} Phone: ${phno} Date: ${date} Time: ${time}`;
+
+  item.textContent = text;
+
+  items.appendChild(item);
 
   document.getElementById("name").value = "";
   document.getElementById("email").value = "";
